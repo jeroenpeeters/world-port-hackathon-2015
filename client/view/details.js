0263502ec7cab476249e24012f87753294944968
unknown <nkhalil@OVSPC270.OVSoftware.local>
2015-09-05 01:20:10 +0200

    limit list to 10 items for now

--- client/view/details.js
+++ client/view/details.js
@@ -1,8 +1,6 @@
 Template.details.helpers({
   allShips: function () {
-    console.log('allShip');
-    var value = Ships.find({}, {speed :{$ne: undefined}}, {limit: 10}).fetch();
-
+    var value = Ships.find({}, {limit: 10}, {speed :{$ne: undefined}}).fetch();
     return value;
   }
 });
