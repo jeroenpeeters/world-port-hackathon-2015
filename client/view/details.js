f621a802466e0d84b87533cb6efc7d10039f7faf
unknown <nkhalil@OVSPC270.OVSoftware.local>
2015-09-05 12:15:06 +0200

    filter added

--- client/view/details.js
+++ client/view/details.js
@@ -1,6 +1,28 @@
 Template.details.helpers({
   allShips: function () {
-    var value = Ships.find({}, {limit: 10}, {speed :{$ne: undefined}}).fetch();
+    var entredname = shipname();
+    console.log('entred name = '+ shipname());
+    if (typeof(entredname) == "undefined") entredname='';
+    var value = Ships.find({"details.shipname" : {$regex : ".*"+entredname+".*"}}).fetch()
+//    Ships.find({"details.shipname" : {$regex : ".*S.*"}}).fetch();
+
     return value;
   }
 });
+
+  shipname = function() {
+    return Session.get('shipname');
+  }
+
+Template.details.events({
+  'keyup #shipname': function(event) {
+        var entredShipName = $(event.currentTarget).val();
+     console.log('entred: ' + $(event.currentTarget).val());
+      Session.set('shipname', entredShipName);
+  }
+  });
+
+
+// 'keyup #shipname': _.throttle(function (evt) {
+//   ...
+// }, 350),
